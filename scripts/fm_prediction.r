0b4a052f73a2104845a23d0a8388c4167c882df6
Simon <simon-schiller@gmx.de>
2015-07-31 18:18:04 +0200

    first working version of FM prediction

--- scripts/fm_prediction.r
+++ scripts/fm_prediction.r
@@ -1,5 +1,233 @@
 
 
-fm_prediction <- function() {
+
+
+fm_execute <- function(trainfile, testfile, outfile, params) {
+    # init parts for libfm execution
+    libfm.cmd <- "libfm -task r"
+    libfm.train <- paste("-train ", trainfile, sep="")
+    libfm.test <- paste("-test ", testfile, sep="")
+    libfm.out <- paste("-out ", outfile, sep="")
+    libfm.dim <- paste("-dim 1,1,", params["k"], sep="")
+    libfm.iter <- paste("-iter ", params["iter"], sep="")
+    libfm.method <- paste("-method ", params["method"], sep="")
+    libfm.stdev <- paste("-init_stdev ", params["stdev"], sep="")
+    libfm.reg <- paste("-regular ", params["reg"], sep="")
+    libfm.lr <- paste("-learn_rate ", params["lr"], sep="")
+    libfm.rlog <- "" #paste("-rlog ", logname, sep="")
     
+    cmd <- paste(libfm.cmd, libfm.train, libfm.test, libfm.out, libfm.dim, libfm.iter, libfm.method, libfm.stdev, libfm.reg, libfm.lr, libfm.rlog, sep=" ")
+    print(paste("command to execute: ", cmd, sep=""))
+    # execute libfm
+    system(cmd)
 }
+
+
+
+fm_prediction <- function(data, data.test, some.params) {
+    # initialize default params
+    params <- list(
+        # method for FM parameter learning
+        method="mcmc",
+        # Anzahl Faktoren
+        k=8,
+        # Anzahl Iterationen
+        iter=400,
+        # output filename
+        stdev=0.1,
+        # regulation (only affect ALS and SGD)
+        reg=0,
+        # learning rate (only affect SGD)
+        lr=0.001
+    )
+    
+    # override default params with those, that are specified in some.params
+    if (!identical(some.params,NA)) {
+        for (n in names(some.params)) {
+            if (!(n %in% names(params))) {
+                stop("unknown parameter: ", n, "\n  allowed parameters: ", paste(names(params),collapse=",")) 
+            }
+            params[n] <- some.params[n]
+        }
+    }
+    
+    # initialize output filename
+    outfile = "tmp/prediction.libfm"
+    
+    #data <- data[,!(names(data) == "Id")]
+    # convert data to matrix
+    data.matrix <- as.matrix(data)
+    
+
+    if(!identical(data.test, NA)) {
+        
+        trainfile <- "tmp/train.libfm"
+        testfile <- "tmp/test.libfm"
+        
+        #test.matrix <- test[,!(names(data) == "Id")]
+        # convert test data to matrix
+        test <- as.matrix(data.test)
+        
+        tcost <- c(rep(0, times=nrow(test)))
+        ecost <- vector('numeric',length(tcost))
+        
+        # create test and training sets
+        train <- data.matrix
+        
+        # define train data
+        # 1: Id, 2: Hazard, 3-ncol: properties
+        x.train <- train[,3:(ncol(train))]
+        y.train <- train[,2]
+        
+        # write the training data as file in sparse libsvm format
+        if(!file.exists(trainfile))
+        {
+            print("write train data in libfm format to file ...")
+            write.matrix.csr(x=x.train, y=y.train, file=trainfile) 
+        }
+        else
+        {
+            print("skip writing train file in libfm format: already exist!")
+        }
+        
+        # define test data
+        # 1: Id, 2-ncol: properties   =>   no hazard in kaggle-test data
+        x.test <- test[,2:(ncol(test))]
+        y.test <- tcost
+        
+        # write the test data as file in sparse libsvm format
+        if(!file.exists(testfile))
+        {
+            print("write test data in libfm format to file ...")
+            write.matrix.csr(x=x.test, y=y.test, file=testfile)
+        }
+        else
+        {
+            print("skip writing test file in libfm format: already exist!")
+        }
+        
+        fm_execute(trainfile=trainfile, testfile=testfile, outfile=outfile, params=params)
+        
+        prediction <- read.table(outfile)
+        ecost <- as.numeric(prediction[,1])
+        
+        # build submission with Id and Hazard
+        ecost.ids <- data.test[["Id"]]
+        submission <- cbind(ecost.ids, ecost)
+        colnames(submission) <- c("Id", "Hazard")
+        
+        # solution is not known
+        solution <- NA
+        
+    }
+    else {
+        
+        tcost <- data[["Hazard"]]
+        ecost <- vector('numeric',length(tcost))
+    
+        # specify strata
+        stratum_count <- 2
+        stratum_size <- nrow(data) %/% stratum_count
+        considered_row_count <- stratum_count * stratum_size
+        i <- 0	
+        while ( i < stratum_count ) {
+            
+            ind <- (i*stratum_size + 1):((i+1)*stratum_size)
+            #ind <- c(1)
+            
+            trainfile <- paste("tmp/train_s", i, ".libfm", sep="")
+            testfile <- paste("tmp/test_s", i, ".libfm", sep="")
+            
+            # cat("Test indices: ", ind[1], "-", ind[length(ind)], "...")
+            
+            # create test and training sets
+            train <- data.matrix[-ind,]
+            test <- data.matrix[ind,]
+            
+            # define train data
+            # 1: Id, 2: Hazard, 3-ncol: properties
+            x.train <- train[,3:(ncol(train))]
+            y.train <- train[,2]
+    
+            # write the training data as file in sparse libsvm format
+            if(!file.exists(trainfile))
+            {
+                print("write train data in libfm format to file ...")
+                write.matrix.csr(x=x.train, y=y.train, file=trainfile) 
+            }
+            else
+            {
+                print("skip writing train file in libfm format: already exist!")
+            }
+            
+            # define test data
+            # 1: Id, 2: Hazard, 3-ncol: properties
+            x.test <- test[,3:(ncol(test))]
+            y.test <- test[,2]
+    
+            # write the test data as file in sparse libsvm format
+            if(!file.exists(testfile))
+            {
+                print("write test data in libfm format to file ...")
+                write.matrix.csr(x=x.test, y=y.test, file=testfile)
+            }
+            else
+            {
+                print("skip writing test file in libfm format: already exist!")
+            }
+    
+            fm_execute(trainfile=trainfile, testfile=testfile, outfile=outfile, params=params)
+            
+            prediction <- read.table(outfile)
+            ecost[ind] <- as.numeric(prediction[,1])
+            i <- i + 1
+        }
+        
+        # build submission with Id and Hazard
+        ecost.ids <- data[["Id"]]
+        submission <- cbind(ecost.ids, ecost)
+        colnames(submission) <- c("Id", "Hazard")
+        
+        # build solution with Id and Hazard
+        tcost.ids <- data[["Id"]]
+        solution <- cbind(tcost.ids, tcost)
+        colnames(solution) <- c("Id", "Hazard")
+    }
+    
+    
+    return(
+        list(
+            hazard=data.frame(true=tcost,predicted=ecost), 
+            params=params, 
+            test=!identical(data.test, NA), 
+            submission=submission, 
+            solution=solution
+            )
+        )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- scripts/prepare_data.r
+++ scripts/prepare_data.r
@@ -1,20 +1,21 @@
 
 
 
-prepare_data <- function() {
+prepare_data <- function(data, column.name.range) {
     
+    # remove columns with negative feature importance
+    negative.feature.importance <- c("T1_V10", "T1_V13", "T2_V7", "T2_V10")
+    data <- data[,!(names(data) %in% negative.feature.importance)]
+    
+    data <- replace_with_indicators(data, column.name.range = column.name.range)
+    
+    return(data)
 }
 
-
-extract_column_ranges <- function(data, column.name.range) {
-    for(column.name in column.nane.range) {
-        extract_column_range(data, column.name = column.name)
-    }
-}
 
 extract_column_range <- function(data, column.name) {
-    different.elements <- unique(data[column.name])
-    write.table(different.elements, file = paste("tmp/", column.name, ".data", sep=""), sep = ",", row.names = FALSE, append = FALSE, col.names = FALSE)
+    different.elements <- unique(data[[column.name]])
+    write.table(different.elements, file = paste("tmp/", column.name, ".csv", sep=""), sep = ",", row.names = FALSE, append = FALSE, col.names = FALSE)
     
     return(different.elements)
 }
@@ -23,46 +24,137 @@ extract_column_range <- function(data, column.name) {
 replace_with_indicators <- function(data, column.name.range) {
     columns.to.drop <- c()
     
+    print("replacing non-numeric columns with indicators ...")
     for(column.name in column.name.range) {
-        # get unique values of current column
-        values.unique <- extract_column_range(data, column.name = column.name)
+        print(paste("curr column: ", column.name, sep=""))
         
+        # get unique values of current column
+        filename <- paste("tmp/", column.name, ".csv", sep="")
+        if(!file.exists(filename))
+        {
+            print("extracting value range and write range to appropriate file ...")
+            unique.value.range <- extract_column_range(data, column.name = column.name)
+        }
+        else
+        {
+            print("reading value range from file ...")
+            unique.value.range <- read.csv(filename, sep = ",", header = FALSE, stringsAsFactors = FALSE)
+            unique.value.range <- as.vector(as.matrix(unique.value.range))
+        }
+        
+        # get index of current column.name
+        idx <- which(names(data) == column.name)[1]
+        
+        # some initializations
+        explanatory_variables <- c()
+        indicator.list <- list()
+        
+        
+        print("parsing current column to indicators ... ")
+        for(unique.value in unique.value.range) {
+            # create vector with zeros
+            vec <- c(rep(0, times=nrow(data)))
+            # replace all lines, where the current unique.value appears
+            vec <- replace(vec, data[column.name] == unique.value, 1)
+            #print(vec)
+            # add appropriate column name
+            curr.name <- paste(column.name, "_", unique.value, sep="")
+            explanatory_variables <- append(explanatory_variables, curr.name)
+            # append vector to indicator list
+            indicator.list[[curr.name]] <- vec
+            #indicator.list <- c(indicator.list, vec)
+        }
+        
+        indicator.data <- do.call("cbind", indicator.list)
+
+        print("adding indicators to data table ... ")
+        # join data and indicator.data
+        data <- cbind(data[,1:idx, drop=F], indicator.data, data[,(idx+1):length(data), drop=F])
+        # remember column.name to drop later
+        columns.to.drop <- c(columns.to.drop, column.name)
         
     }
     
-    # TODO: replace non numeric values with indicator vectors
-    param <- "ENGINE_TYPE_GROUP"
-    if(param %in% params$metadata) {
-        engine.types.range <- c("BENZIN", "DIESEL", "GAS", "NA")
+    print("dropping non-numeric columns ...")
+    data <- data[,!(names(data) %in% columns.to.drop)]
+    
+    
+    return(data)
+}
+
+
+
+
+replace_with_id <- function(data, column.name.range) {
+    columns.to.drop <- c()
+    
+    print("replacing non-numeric columns with indicators ...")
+    for(column.name in column.name.range) {
+        print(paste("curr column: ", column.name, sep=""))
         
-        # append engine types (names) to regions matrix
-        explanatory_variables <- append(explanatory_variables, param)
-        regions <- cbind(regions, car.model[param])
-        
-        # engine.types.vectors <- list()
-        for(engine.type in engine.types.range) {
-            if(engine.type != "NA") {
-                # create vector with zeros
-                vec <- c(rep(0, times=nrow(regions)))
-                # set appropriate indices to one
-                vec <- replace(vec, regions[param]==engine.type, 1)
-                # append vec as column to regions
-                regions <- cbind(regions, vec)
-                # add explanatory variable
-                explanatory_variables <- append(explanatory_variables, paste("ET_", engine.type, sep=""))
-            }
-            else {
-                # create vector with zeros
-                vec <- c(rep(0, times=nrow(regions)))
-                # set appropriate indices to one
-                vec <- replace(vec, is.na(regions[param]), 1)
-                # append vec as column to regions
-                regions <- cbind(regions, vec)
-                # add explanatory variable
-                explanatory_variables <- append(explanatory_variables, paste("ET_", engine.type, sep=""))
-            }
+        # get unique values of current column
+        filename <- paste("tmp/", column.name, ".csv", sep="")
+        if(!file.exists(filename))
+        {
+            print("extracting value range and write range to appropriate file ...")
+            unique.value.range <- extract_column_range(data, column.name = column.name)
+            print("done!")
+        }
+        else
+        {
+            print("reading value range from file ...")
+            unique.value.range <- read.csv(filename, sep = ",", header = FALSE, stringsAsFactors = FALSE)
+            unique.value.range <- as.vector(as.matrix(unique.value.range))
+            print("done!")
         }
         
-        columns.to.drop <- c(columns.to.drop, param)
+        # get index of current column.name
+        idx <- which(names(data) == column.name)[1]
+        
+        # some initializations
+        explanatory_variables <- c()
+        indicator.list <- list()
+        
+        
+        print("parsing current column to indicators ... ")
+        for(unique.value in unique.value.range) {
+            # create vector with zeros
+            vec <- c(rep(0, times=nrow(data)))
+            # replace all lines, where the current unique.value appears
+            vec <- replace(vec, data[column.name] == unique.value, 1)
+            #print(vec)
+            # add appropriate column name
+            curr.name <- paste(column.name, "_", unique.value, sep="")
+            explanatory_variables <- append(explanatory_variables, curr.name)
+            # append vector to indicator list
+            indicator.list[[curr.name]] <- vec
+            #indicator.list <- c(indicator.list, vec)
+        }
+        
+        indicator.data <- do.call("cbind", indicator.list)
+        
+        print("adding indicators to data table ... ")
+        # join data and indicator.data
+        data <- cbind(data[,1:idx, drop=F], indicator.data, data[,(idx+1):length(data), drop=F])
+        # remember column.name to drop later
+        columns.to.drop <- c(columns.to.drop, column.name)
+        
     }
+    
+    print("dropping non-numeric columns ...")
+    data <- data[,!(names(data) %in% columns.to.drop)]
+    
+    print("done!")
+    
+    return(data)
 }
+
+
+
+
+
+
+
+
+
+
